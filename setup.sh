6292009ab957d4171405c5ff14cc2ef1f46c029b
jaidenagrimminck <jaidng1@gmail.com>
2023-05-23 01:04:53 -0700

    bash script update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 mkdir libs
 cd libs
-echo "Downloading version 2023.2.1 of 8840-utils.jar. If this is not the latest version, please update this script."
-curl -o "8840utils.jar" 'https://github.com/frc8840/8840-utils/releases/download/v2023.2.1/8840-utils.jar'
-echo "Installing..."
 sleep 1
+echo "\n"
+echo "Downloading version 2023.2.1 of 8840-utils.jar. If this is not the latest version, please update this script."
+sleep 3
+curl -LJO 'https://github.com/frc8840/8840-utils/releases/download/v2023.2.1/8840-utils.jar'
+echo "\n"
+sleep 1
+echo "Installing..."
+sleep 2
 echo "Done!"
+echo "\n\n"
 cd ..
